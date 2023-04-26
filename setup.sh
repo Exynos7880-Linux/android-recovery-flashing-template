edb2684496f0ae74c425bf070725ef68c561e6ec
aymanrgab <aymanrar2c@gmail.com>
2023-04-26 23:28:13 +0200

    flashing kernel as well

--- setup.sh
+++ setup.sh
@@ -15,28 +15,6 @@ ui_print "Resizing rootfs to 8GB";
 e2fsck -fy /data/rootfs.img
 resize2fs -f /data/rootfs.img 8G
 
-mkdir /s;
-mkdir /r;
-
-# mount droidian rootfs
-mount /data/rootfs.img /r;
-
-# mount android gsi
-mount /r/var/lib/lxc/android/android-rootfs.img /s
-
-# Set udev rules
-ui_print "Setting udev rules";
-cat /s/ueventd*.rc /vendor/ueventd*.rc | grep ^/dev | sed -e 's/^\/dev\///' | awk '{printf "ACTION==\"add\", KERNEL==\"%s\", OWNER=\"%s\", GROUP=\"%s\", MODE=\"%s\"\n",$1,$3,$4,$2}' | sed -e 's/\r//' > /data/70-droidian.rules;
-
-# umount android gsi
-umount /s;
-
-# move udev rules inside rootfs
-mv /data/70-droidian.rules /r/etc/udev/rules.d/70-$VENDOR_DEVICE_PROP.rules;
-
-# umount rootfs
-umount /r;
-
 # halium initramfs workaround,
 # create symlink to android-rootfs inside /data
 if [ ! -e /data/android-rootfs.img ]; then
